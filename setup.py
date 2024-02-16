4d3ffb87f8ecd37dc7cf0df95d86d8b0ca56c1cd
Michael Hansen <mike@rhasspy.org>
2024-02-16 14:02:12 -0600

    Add tox

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     keywords="home assistant intent recognition",
 )
